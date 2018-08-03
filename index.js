396e86e58de1e7af200f193d22632457417d9058
Priyanka Jain <Priyanka.Jain2@netapp.com>
2018-08-03 09:25:00 -0400

    navigation

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import { AppRegistry } from 'react-native';
 import { StyleProvider } from 'native-base';
 import { name as appName } from './app.json';
 import App from './src/App';
-import getTheme from './src/theme/components';
-import variables from './src/theme/variables/commonColor';
+// import getTheme from './src/theme/components';
+// import variables from './src/theme/variables/commonColor';
 
 const Setup = () => (
-	<StyleProvider style={getTheme(variables)}>
+
 		<App />
-	</StyleProvider>
+
 );
 
 AppRegistry.registerComponent(appName, () => Setup);
