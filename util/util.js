9a0082f0935992f70e4db48f9bbc084d4a41dd9f
bobby569 <bobby.yczh@gmail.com>
2018-08-03 09:48:56 -0400

    add karthik implementation

--- util/util.js
+++ util/util.js
@@ -4,6 +4,18 @@ const CREATE_QR_BASE = 'https://api.qrserver.com/v1/create-qr-code/';
 const READ_QR_BASE = 'http://api.qrserver.com/v1/read-qr-code/';
 const PROFILE_BASE = 'http://cedprod.corp.netapp.com:10080/search?';
 
+function HTMLParser(rawHTML) {
+	const pos = rawHTML.split('class="text12">');
+	return {
+		name: pos[2].split('<')[0],
+		userName: pos[6].split('<')[0],
+		cubicle: pos[8].split('<')[0],
+		location: pos[10].split('<')[0],
+		designation: pos[15].split('<')[0],
+		address: pos[16].split('<')[0]
+	};
+}
+
 async function createQR(username) {
 	return await axios.get(`${CREATE_QR_BASE}?size=200*200&data=${username}`);
 }
@@ -12,8 +24,8 @@ async function readQR(fileurl) {
 	return await axios.get(`${READ_QR_BASE}?fileurl=${fileurl}`);
 }
 
-async function queryHTML(index = 26, username = wnatasha) {
-	const url = `${PROFILE_BASE}index=${index}&userName=${username}`;
-	const res = await axios.get(url);
-	return res.data;
+async function queryHTML(username = wnatasha) {
+	const url = `${PROFILE_BASE}userName=${username}`;
+	const rawHTML = await axios.get(url);
+	return HTMLParser(rawHTML);
 }
